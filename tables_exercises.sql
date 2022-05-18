83d8fcb138975ada58cf6b4405e8d9d3ad20fbd0
JeremyLagunas <jeremy.lagunas@gmail.com>
2022-05-18 15:26:29 -0500

    commit for tables exercises

---
+++ tables_exercises.sql
@@ -0,0 +1,12 @@
+use employees;
+
+SHOW tables;
+
+DESCRIBE employees;
+
+-- 6. numeric type column: emp_no
+-- 7. string type: first_name, last_name
+-- 8. date type: birth_date, hire_date
+-- 9. Relationship between employees and dept tables: the dept table has the emp_no as a column. 
+
+SHOW CREATE TABLE dept_manager;
